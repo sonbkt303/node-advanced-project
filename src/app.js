ead313021e50f544056dc728ea714c14e0c8967c
Mike <mike.bui@ewoosoft.com>
2023-04-21 11:16:27 +0700

    apply expressMiddleware to app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
-// import express from "express";
+import express from "express";
 // import { graphqlHTTP } from "express-graphql";
 // import { buildSchema } from "graphql";
 dotenv.config();
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+
 
 // const expressPlayground = require('graphql-playground-middleware-express')
 // .default
@@ -51,21 +57,56 @@ const resolvers = {
   },
 };
 
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully
+const httpServer = http.createServer(app);
 
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+// Ensure we wait for our server to start
+await server.start();
 
-const { url } = await startStandaloneServer(server, {
-  context: async ({ req }) => {
-    console.log('req', req.headers.token);
-    return { token: req.headers.token }
-  },
-  listen: { port: PORT_GRAPHQL },
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const { url } = await startStandaloneServer(server, {
+//   context: async ({ req }) => {
+//     console.log('req', req.headers.token);
+//     return { token: req.headers.token }
+//   },
+//   listen: { port: PORT_GRAPHQL },
+// });
+
+
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors,
+  // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+  bodyParser.json({ limit: '50mb' }),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
+
+// Modified server startup
+await new Promise((resolve) => {
+  return httpServer.listen({ port: 4000 }, resolve)
 });
-
-console.log(`🚀  Server ready at ${url}`);
+console.log(`🚀 Server ready at http://localhost:4000/`);
 
 
 // app.get("/", (req, res) => {
