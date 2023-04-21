71a819c1aa2b171cacb60c6bb327a5c7a2ec3be5
Mike <mike.bui@ewoosoft.com>
2023-04-21 13:51:07 +0700

    Add scalar example to project

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,3 +1,41 @@
+import { GraphQLScalarType, Kind, GraphQLError } from 'graphql';
+
+const dateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  serialize(value) {
+    if (value instanceof Date) {
+      return value.getTime(); // Convert outgoing Date to integer for JSON
+    }
+    throw Error('GraphQL Date Scalar serializer expected a `Date` object');
+  },
+  parseValue(value) {
+    if (typeof value === 'number') {
+      return new Date(value); // Convert incoming integer to Date
+    }
+    throw new Error('GraphQL Date Scalar parser expected a `number`');
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      // Convert hard-coded AST string to integer and then to Date
+      return new Date(parseInt(ast.value, 10));
+    }
+    // Invalid hard-coded value (not an integer)
+    return null;
+  },
+});
+
+// Validation function for checking "oddness"
+function oddValue(value) {
+  console.log('value', value);
+  if (typeof value === 'number' && Number.isInteger(value) && value % 2 !== 0) {
+    return value;
+  }
+  throw new GraphQLError('Provided value is not an odd integer', {
+    extensions: { code: 'BAD_USER_INPUT' },
+  });
+}
+
 const books = [
   {
     title: "The Awakening",
@@ -10,6 +48,11 @@ const books = [
 ];
 
 export const typeDefs = `#graphql
+
+    scalar MyCustomScalar
+    scalar Date  # highlight-line 
+    scalar Odd
+
     interface MutationResponse {
       code: String!
       success: Boolean!
@@ -71,6 +114,7 @@ export const typeDefs = `#graphql
       books: [Book]
       author: [Author]
       avatar(borderColor: AllowedColor): UpdateUserEmailMutationResponse # enum argument
+      echoOdd(odd: Odd!): Odd!
     }
 
     type Mutation {
@@ -85,6 +129,23 @@ export const resolvers = {
     GREEN: '#0f0',
     BLUE: '#00f',
   },
+  Odd: new GraphQLScalarType({
+    name: 'Odd',
+    description: 'Odd custom scalar type',
+    parseValue: oddValue,
+    serialize: oddValue,
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return oddValue(parseInt(ast.value, 10));
+      }
+      throw new GraphQLError('Provided value is not an odd integer', {
+        extensions: { code: 'BAD_USER_INPUT' },
+      });
+    },
+  }),
+  Date: dateScalar,
+
+
   Query: {
     books: () => books,
     avatar: (parent, args) => {
@@ -97,6 +158,9 @@ export const resolvers = {
         message: "12312312"
       };
       // args.borderColor is '#f00', '#0f0', or '#00f'
+    },
+    echoOdd(_, { odd }) {
+      return odd;
     },
   },
 };
