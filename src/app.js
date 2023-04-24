dc0456b46c8a017aef8cc45c89ac0fc5aab7bfb9
Mike <mike.bui@ewoosoft.com>
2023-04-24 13:37:03 +0700

    Remove comment

--- src/app.js
+++ src/app.js
@@ -9,69 +9,12 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { expressMiddleware } from "@apollo/server/express4";
-import mongoose from "mongoose";
-// import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
-// import { typeDefs, upperDirectiveTransformer } from "./schema/index.js";
-// import { resolvers } from "./schema/resolvers.js";
-// import { typeDefs } from "./schema/typeDefs.js";
-// import { upperDirectiveTransformer } from "./schema/directive.js";
 import schema from "./schema/index.js";
 import MyDatabase from "./datasource/myDatabase.js";
 import mongodbConnection from "./datasource/mongodb.js";
 import config from "./config/index.js";
 
 const postgresqlConnection = new MyDatabase(config.dataSources.postgresql);
-
-// mongoose.connect("mongodb://127.0.0.1:27017/simple_db", {
-//   // autoIndex: false, // Don't build indexes
-//   // maxPoolSize: 10, // Maintain up to 10 socket connections
-//   // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-//   // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-//   // family: 4, // Use IPv4, skip trying IPv
-// });
-
-// const UserSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   age: {
-//     type: Number,
-//     default: 0,
-//   },
-// });
-
-// const User = mongoose.model("User", UserSchema, "users");
-
-// const user = new User({ name: "mike bui", age: 18 });
-
-// // user.save();
-// const mongodbConnection = mongoose.connection;
-
-// mongodbConnection.on("error", console.error.bind(console, "connection error: "));
-// mongodbConnection.once("open", async  function () {
-//   console.log("Connected successfully");
-// });
-
-// const main = async () => {
-//   // const connection = await mongodb;
-//   // const data = await User.aggregate([
-//   //   {
-//   //     $match: {},
-//   //   },
-//   // ]);
-//   let data = await mongoose.connection.model('User').find()
-//   console.log(data);
-
-//   // console.log(connection);
-//   // const data = await mongodb.collection('students').find();
-//   // console.log('data', data);
-// };
-
-// main();
-
-// const expressPlayground = require('graphql-playground-middleware-express')
-// .default
 
 const PORT = process.env.PORT || 3000;
 const PORT_GRAPHQL = process.env.PORT_GRAPHQL || 4000;
